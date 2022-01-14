5a23d35ae00daa29bb023ae6829f65737427dc3d
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-01-14 20:37:58 +0100

    Add colors for fishes and when user click fish, fish will escape

--- aquua/js/game.js
+++ aquua/js/game.js
@@ -19,15 +19,32 @@ game.height = gameConfig.windowSize.height;
 // Fishes objects
 let fishes = [];
 
+// Fishes colors
+const colors = ["red", "blue", "green", "black"];
+
+// Events
+game.addEventListener("mousedown", (e) => {
+    let rect = game.getBoundingClientRect();
+    let mousePos = { x: e.clientX - rect.left, y: e.clientY - rect.top };
+    fishes.forEach((fish) => {
+        if (mousePos.x <= fish.posX + fish.r && mousePos.x >= fish.posX - fish.r &&
+            mousePos.y <= fish.posY + fish.r && mousePos.y >= fish.posY - fish.r) {
+            fish.setNewFollowPoint(mousePos.x, mousePos.y);
+            fish.confusion(2000);   
+        }
+    });
+});
+
 // Init function to create objects when game start
 function init() {
     // Create fishes
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 20; i++) {
         fishes.push(new Fish(
             ctx,
             randomNumber(0, ctx.canvas.width - 15),
             randomNumber(0, ctx.canvas.height - 15),
-            15
+            15,
+            colors[randomNumber(0, colors.length - 1)]
         ));
     }
 }
