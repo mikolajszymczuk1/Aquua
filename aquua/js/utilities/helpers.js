6d077725201edc823f2a8488167f534540805845
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-01-14 19:00:27 +0100

    Create randomNumber helper function

---
+++ aquua/js/utilities/helpers.js
@@ -0,0 +1,7 @@
+// Return random intiger number in range(min, max), max inclusive
+export function randomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
