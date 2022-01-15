848602079513855e78dbe3fa7adb47bffb0b5c5f
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-01-15 03:37:51 +0100

    Add hunger status and hunger status bar

--- aquua/js/gameClasses/fish.js
+++ aquua/js/gameClasses/fish.js
@@ -9,6 +9,11 @@ class Fish {
         this.color = color;
         this.escape = false;
         this.hasFoodTarget = false;
+        this.showInfo = false;
+
+        // Hunger status
+        this.hungerState = 0;
+        this.maxHungerState = 30;
 
         // Create first point after fish is created
         this.followPoint = this.createFollowPoint();
@@ -19,6 +24,22 @@ class Fish {
         this.c.arc(this.posX, this.posY, this.r, 0, 2 * Math.PI);
         this.c.fillStyle = this.color;
         this.c.fill();
+
+        // Draw hunger bar
+        if (this.showInfo) {
+            let barWidth = 30;
+    
+            this.c.beginPath();
+            this.c.strokeStyle = "white";
+            this.c.rect(this.posX - (barWidth / 2), this.posY - 25, barWidth, 5);
+            this.c.stroke();
+    
+            this.c.beginPath();
+            let barValue = parseInt((this.hungerState / this.maxHungerState) * 100);
+            this.c.fillStyle = "white";
+            this.c.fillRect(this.posX - 15, this.posY - 25, (barValue * barWidth) / 100, 5);
+            this.c.stroke();
+        }
     }
 
     update() {
@@ -70,24 +91,27 @@ class Fish {
     }
 
     catchFood(foodsArray) {
-        foodsArray.forEach((food) => {
-            // When food is near the fish will start chasing the food
-            if (!this.hasFoodTarget) {
-                let eatOffset = 100;
-                if (food.posX > (this.posX - eatOffset) && food.posX < (this.posX + eatOffset) &&
-                    food.posY > (this.posY - eatOffset) && food.posY < (this.posY + eatOffset)) {
-                    this.hasFoodTarget = true;
-                    this.setNewFollowPoint(food.posX, food.posY);
+        if (this.hungerState < this.maxHungerState) {
+            foodsArray.forEach((food) => {
+                // When food is near the fish will start chasing the food
+                if (!this.hasFoodTarget) {
+                    let eatOffset = 100;
+                    if (food.posX > (this.posX - eatOffset) && food.posX < (this.posX + eatOffset) &&
+                        food.posY > (this.posY - eatOffset) && food.posY < (this.posY + eatOffset)) {
+                        this.hasFoodTarget = true;
+                        this.setNewFollowPoint(food.posX, food.posY);
+                    }
                 }
-            }
-
-            // When colide with food, destroy food
-            if (food.posX > (this.posX - this.r) && food.posX < (this.posX + this.r) &&
-                food.posY > (this.posY - this.r) && food.posY < (this.posY + this.r)) {
-                food.isEaten = true;
-                this.hasFoodTarget = false;
-            }
-        });
+    
+                // When colide with food, destroy food
+                if (food.posX > (this.posX - this.r) && food.posX < (this.posX + this.r) &&
+                    food.posY > (this.posY - this.r) && food.posY < (this.posY + this.r)) {
+                    food.isEaten = true;
+                    this.hasFoodTarget = false;
+                    this.hungerState += 5;
+                }
+            });
+        }
     }
 }
 
