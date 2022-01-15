1af896c0fad3e574413e2d4e8225d6c4653249bd
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-01-15 01:33:56 +0100

    Update and draw food

--- aquua/js/game.js
+++ aquua/js/game.js
@@ -1,8 +1,11 @@
 import Fish from "./gameClasses/fish";
+import Food from "./gameClasses/food";
 import { randomNumber } from "./utilities/helpers";
 
 const game = document.getElementById("game");
 const ctx = game.getContext("2d");
+
+game.addEventListener("contextmenu", (e) => { e.preventDefault() });
 
 // Config
 const gameConfig = {
@@ -19,6 +22,9 @@ game.height = gameConfig.windowSize.height;
 // Fishes objects
 let fishes = [];
 
+// Foods
+let foods = [];
+
 // Fishes colors
 const colors = ["red", "blue", "green", "black"];
 
@@ -26,13 +32,22 @@ const colors = ["red", "blue", "green", "black"];
 game.addEventListener("mousedown", (e) => {
     let rect = game.getBoundingClientRect();
     let mousePos = { x: e.clientX - rect.left, y: e.clientY - rect.top };
-    fishes.forEach((fish) => {
-        if (mousePos.x <= fish.posX + fish.r && mousePos.x >= fish.posX - fish.r &&
-            mousePos.y <= fish.posY + fish.r && mousePos.y >= fish.posY - fish.r) {
-            fish.setNewFollowPoint(mousePos.x, mousePos.y);
-            fish.confusion(2000);   
-        }
-    });
+    
+    // When click fish, fish get confused
+    if (e.button === 0) {
+        fishes.forEach((fish) => {
+            if (mousePos.x <= fish.posX + fish.r && mousePos.x >= fish.posX - fish.r &&
+                mousePos.y <= fish.posY + fish.r && mousePos.y >= fish.posY - fish.r) {
+                fish.setNewFollowPoint(mousePos.x, mousePos.y);
+                fish.confusion(2000);
+            }
+        });
+    }
+
+    // When click on free space/area, create food
+    if (e.button === 2) {
+        foods.push(new Food(ctx, mousePos.x, mousePos.y, 5));
+    }
 });
 
 // Init function to create objects when game start
@@ -53,7 +68,24 @@ function init() {
 function animate() {
     window.requestAnimationFrame(animate);
     ctx.clearRect(0, 0, game.width, game.height)
-    fishes.forEach((fish) => { fish.draw(); fish.update() });
+
+    // Update fishes
+    fishes.forEach((fish) => {
+        fish.draw();
+        fish.update();
+        fish.catchFood(foods);
+    });
+    
+    // Update foods
+    foods.forEach((food) => {
+        food.draw();
+        food.update();
+        
+        if (food.isOutOfAquarium() || food.isEaten) {
+            let newFoods = foods.filter((el) => el !== food);
+            foods = newFoods;
+        }
+    });
 }
 
 init();
